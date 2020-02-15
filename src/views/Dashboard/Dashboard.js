96f8802011209d1ad11127e0303e6542d9b963ee
DrZin <pasawee.dev@gmail.com>
2020-02-15 19:42:18 +0700

    delete unuse

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
-import Web3 from 'web3';
+
 import {
   Budget,
   TotalUsers,
@@ -19,49 +19,9 @@ const useStyles = makeStyles(theme => ({
 
 const Dashboard = () => {
   const classes = useStyles();
-  const [data, setData] = useState({});
 
-  let address = '0x52b04c346266aa417F6f3Ef7553732782B355404';
-  let abi = [
-    {
-      inputs: [],
-      name: 'get',
-      outputs: [
-        {
-          internalType: 'uint256',
-          name: '',
-          type: 'uint256'
-        }
-      ],
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      inputs: [
-        {
-          internalType: 'uint256',
-          name: 'x',
-          type: 'uint256'
-        }
-      ],
-      name: 'set',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function'
-    }
-  ];
-  useEffect(() => {
-    const fetchData = async () => {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-      const accounts = await web3.eth.getAccounts();
-      const contract = new web3.eth.Contract(abi, address);
-    };
-    fetchData();
-  }, []);
   return (
     <div className={classes.root}>
-      {console.log(data)}
-
       <Grid
         container
         spacing={4}
