afa5dd8cab2ad12f2c4e03ed02821978e8431603
DrZin <pasawee.dev@gmail.com>
2020-02-15 20:27:12 +0700

    can fetch

--- src/views/Dashboard/components/LatestOrders/LatestOrders.js
+++ src/views/Dashboard/components/LatestOrders/LatestOrders.js
@@ -26,6 +26,7 @@ import {
   Typography,
   Grid
 } from '@material-ui/core';
+import Web3 from 'web3';
 
 import CloseIcon from '@material-ui/icons/Close';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
@@ -63,11 +64,228 @@ const statusColors = {
 const LatestOrders = props => {
   const { className, ...rest } = props;
   const classes = useStyles();
+
   const [orders] = useState(JsonMock);
+
   const [open, setOpen] = React.useState(false);
+  const [data, setData] = React.useState({});
+
+  const abi = [
+    {
+      constant: true,
+      inputs: [],
+      name: 'mintingFinished',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'name',
+      outputs: [{ name: '', type: 'string' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        { name: '_spender', type: 'address' },
+        { name: '_value', type: 'uint256' }
+      ],
+      name: 'approve',
+      outputs: [],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [{ name: '', type: 'uint256' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        { name: '_from', type: 'address' },
+        { name: '_to', type: 'address' },
+        { name: '_value', type: 'uint256' }
+      ],
+      name: 'transferFrom',
+      outputs: [],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'decimals',
+      outputs: [{ name: '', type: 'uint256' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'unpause',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        { name: '_to', type: 'address' },
+        { name: '_amount', type: 'uint256' }
+      ],
+      name: 'mint',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'paused',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [{ name: '_owner', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ name: 'balance', type: 'uint256' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'finishMinting',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'pause',
+      outputs: [{ name: '', type: 'bool' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'owner',
+      outputs: [{ name: '', type: 'address' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'symbol',
+      outputs: [{ name: '', type: 'string' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        { name: '_to', type: 'address' },
+        { name: '_value', type: 'uint256' }
+      ],
+      name: 'transfer',
+      outputs: [],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [
+        { name: '_to', type: 'address' },
+        { name: '_amount', type: 'uint256' },
+        { name: '_releaseTime', type: 'uint256' }
+      ],
+      name: 'mintTimelocked',
+      outputs: [{ name: '', type: 'address' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        { name: '_owner', type: 'address' },
+        { name: '_spender', type: 'address' }
+      ],
+      name: 'allowance',
+      outputs: [{ name: 'remaining', type: 'uint256' }],
+      payable: false,
+      type: 'function'
+    },
+    {
+      constant: false,
+      inputs: [{ name: 'newOwner', type: 'address' }],
+      name: 'transferOwnership',
+      outputs: [],
+      payable: false,
+      type: 'function'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, name: 'to', type: 'address' },
+        { indexed: false, name: 'value', type: 'uint256' }
+      ],
+      name: 'Mint',
+      type: 'event'
+    },
+    { anonymous: false, inputs: [], name: 'MintFinished', type: 'event' },
+    { anonymous: false, inputs: [], name: 'Pause', type: 'event' },
+    { anonymous: false, inputs: [], name: 'Unpause', type: 'event' },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, name: 'owner', type: 'address' },
+        { indexed: true, name: 'spender', type: 'address' },
+        { indexed: false, name: 'value', type: 'uint256' }
+      ],
+      name: 'Approval',
+      type: 'event'
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, name: 'from', type: 'address' },
+        { indexed: true, name: 'to', type: 'address' },
+        { indexed: false, name: 'value', type: 'uint256' }
+      ],
+      name: 'Transfer',
+      type: 'event'
+    }
+  ];
+  const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
+  const rpcURL =
+    'https://mainnet.infura.io/v3/35d85ee041f8443ba0b37fad6d7e8332';
+
+  const web3 = new Web3(rpcURL);
+  web3.eth.getAccounts().then(console.log);
+
+  
+  const fetchData = () => {
+    const contract = new web3.eth.Contract(abi, address);
+    contract.methods.name().call((err, result) => {
+      setData({ name: result });
+    });
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
+    fetchData();
   };
 
   const handleClose = () => {
@@ -187,8 +405,7 @@ const LatestOrders = props => {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+            {data.name}
           </DialogContentText>
         </DialogContent>
       </Dialog>
