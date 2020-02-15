b5dfbbba130d553e923a38d36505406863e9f3df
DrZin <pasawee.dev@gmail.com>
2020-02-15 17:51:55 +0700

    last update notification

--- src/components/notification/notificationContainer.js
+++ src/components/notification/notificationContainer.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Paper } from '@material-ui/core';
-
+import {
+  Paper,
+  CardHeader,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import { Inbox as InboxIcon, Drafts as DraftsIcon } from '@material-ui/icons';
 const useStyles = makeStyles(theme => ({
   root: {
-    height: '100%',
-    boxShadow:
-      '9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px rgba(255,255,255, 0.5)'
+    maxWidth: '350px',
+    height: '30%'
   }
 }));
+
+const data = [
+  { text: 'inbox ' },
+  {
+    text: 'email'
+  }
+];
+
 const NotificationsContainer = props => {
   const { className, ...rest } = props;
   const classes = useStyles();
-  return <Paper
-    {...rest}
-    className={clsx(classes.root, className)}
-         ></Paper>;
+  return (
+    <Paper
+      {...rest}
+      className={clsx(classes.root, className)}
+      tabIndex="-1"
+    >
+      <CardHeader
+        tabIndex="-2"
+        title={'notifications'}
+      />
+      <List
+        aria-label="main mailbox folders"
+        component="nav"
+      >
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+    </Paper>
+  );
 };
+
+export default NotificationsContainer;
--- src/stories/2-notification.stories.js
+++ src/stories/2-notification.stories.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import Noti from '../components/notification/notificationContainer.js';
 
+export default {
+  title: 'Noti',
+  component: Noti
+};
+
+export const NotiCard = () => {
+  return <Noti />;
+};
